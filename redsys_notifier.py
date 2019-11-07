eb3da8a4d9623bfe294cfb2264cc20a032b926a9
Xavier Martínez <xavier1997@gmail.com>
2019-11-07 08:33:30 +0100

    Initial commit

---
+++ redsys_notifier.py
@@ -0,0 +1,44 @@
+import json
+import os
+
+import requests
+from datetime import date, timedelta
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass  # dotenv is only used in dev envs
+
+USERNAME = os.getenv("REDSYS_USERNAME")
+PASSWORD = os.getenv("REDSYS_PASSWORD")
+
+headers = {'Content-Type': 'application/json;charset=UTF-8'}
+data = {"username": USERNAME, "password": PASSWORD}
+response = requests.post('https://canales.redsys.es/admincanales-web/services/usuarios/login', headers=headers, data=json.dumps(data))
+
+headers['admincanales-auth-token'] = response.json()['token']
+
+today = date.today()
+yesterday = today - timedelta(days=1)
+today_date = today.strftime("%d-%m-%Y")
+yesterday_date = yesterday.strftime("%d-%m-%Y")
+
+data = {"data": {"comercio": USERNAME, "terminal": None, "fechaIni": yesterday_date, "fechaFin": today_date,
+                 "tipo": None, "resultado": None, "horaIni": "00:00:00", "horaFin": "23:59:59", "pedido": None,
+                 "order": "fechaNotificacion,horaNotificacion", "direction": "ASC", "tempHoraInicio": "0",
+                 "tempMinutoInicio": "0", "tempHoraFin": "23", "tempMinutoFin": "59"},
+        "page": 0, "size": "100", "order": "fechaNotificacion,horaNotificacion", "direction": "ASC"}
+
+nots = requests.post('https://canales.redsys.es/admincanales-web/services/notificaciones/consulta', headers=headers, data=json.dumps(data)).json()
+if nots['numberOfElements'] != nots['totalElements']:
+    raise Exception("More elements than page size")
+
+unsuccessful_notifications = [notif['detalleResultado'] for notif in nots['content'] if notif['resultadoNotificacion'] != 'S']
+if len(unsuccessful_notifications) != 0:
+    for un in unsuccessful_notifications:
+        print(un)
+    raise Exception("fml")  # Hopefully if the actions fails Github will notify me, so I don't have to implement it.
+print("No unsuccessful_notifications found")
+
+
